d5fb4d7a58716b44a8943808b5db0a4800bf1ce8
d1m0n4ik <svyryd2012@gmail.com>
2023-06-17 17:32:30 +0300

    Home page

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,4 +1,9 @@
 const Home = () => {
-   return <div></div>
+   return (
+      <div className="flex justify-between items-center">
+         <div className="text-8xl font-medium">Welcome to our website</div>
+         <img src="https://cdn-icons-png.flaticon.com/512/1809/1809216.png" alt="intro" />
+      </div>
+   )
 }
 export default Home
